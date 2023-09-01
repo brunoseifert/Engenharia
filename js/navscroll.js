88da30c0f83a25e74dd6b264f78a35d767911248
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-09-01 16:09:37 -0300

    add style transition

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,8 +1,9 @@
 var nav = document.querySelector('nav');
 
       window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
+        if (window.pageYOffset > 20) {
           nav.classList.add('bg-dark', 'shadow');
+          nav.style.transition = 'all 0.5s';
         } else {
           nav.classList.remove('bg-dark', 'shadow');
         }
